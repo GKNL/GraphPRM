d76a0e5544e110e2ba5c5ebcd0e75b76ad426d0e
pengmiao <pengmiao@whu.edu.cn>
2025-02-20 00:58:25 +0800

    SFT_graphPRM

---
+++ prm/code/finetune_qwen_SFT.py
@@ -0,0 +1,241 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
+import torch
+from datasets import load_dataset
+import argparse
+import os
+import time
+
+from peft import PeftModel
+from peft import get_peft_model, LoraConfig, TaskType
+# Ensure bitsandbytes is available for 8-bit quantization
+# import bitsandbytes as bnb
+from sklearn.metrics import roc_auc_score, log_loss, accuracy_score
+
+from torch.nn import BCEWithLogitsLoss
+from transformers import DataCollatorWithPadding
+from datasets import concatenate_datasets
+
+import random
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_path", type=str, default="hugging_cache/Qwen2.5-Math-7B-Instruct")
+parser.add_argument("--data_path", type=str, default="../../data/GraphSilo")
+parser.add_argument("--per_device_train_batch_size", type=int, default=2)  # 4
+parser.add_argument("--per_device_eval_batch_size", type=int, default=2)  # 4
+parser.add_argument("--total_batch_size", type=int, default=64)  # 16
+parser.add_argument("--learning_rate", type=float, default=1e-6)  # 1e-4
+parser.add_argument("--datasets", type=str, default='all')
+parser.add_argument("--server", type=str, default='SFT_GraphPRM_7B')
+
+
+args = parser.parse_args()
+
+
+
+good_token = '+'
+bad_token = '-'
+step_tag = '\n\n\n\n\n' #ки
+step_tag2 = '\n\n'
+
+model_path = args.model_path
+
+# tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+tokenizer = AutoTokenizer.from_pretrained(
+    model_path, 
+    add_eos_token=False, 
+)
+
+
+print(tokenizer.eos_token_id)
+
+tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos token
+tokenizer.padding_side = "left"  # Allow batched inference
+
+
+# tokenizer = AutoTokenizer.from_pretrained('peiyi9979/math-shepherd-mistral-7b-prm')
+candidate_tokens = tokenizer.encode(f" {good_token} {bad_token}") # [488, 481]
+print(candidate_tokens)
+step_tag_id = tokenizer.encode(f" {step_tag}")[-1] # 76325
+print('step_tag_id:',tokenizer.encode(f" {step_tag}"))
+print('step_tag_id2:',tokenizer.encode(f"{step_tag2}"))
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_path,
+    # load_in_8bit=True,   # Enables 8-bit quantization
+    # device_map="auto",   # Automatically assigns the model to available GPUs/CPUs
+    # torch_dtype=torch.float16,  # Mixed precision for faster inference
+    torch_dtype=torch.bfloat16,
+    # attn_implementation="flash_attention_2",
+)
+
+# for name,param in model.named_parameters():
+#     print(name)
+# print(model)
+
+"""LORA"""
+# lora_config = LoraConfig(
+#     task_type=TaskType.CAUSAL_LM,  # LoRA for causal language modeling task
+#     r=8,  # Rank of LoRA
+#     lora_alpha=32,  # Alpha scaling factor for LoRA
+#     lora_dropout=0.1,  # Dropout rate for LoRA layers
+#     target_modules=["q_proj", "v_proj"],  # Apply LoRA to specific layers
+# )
+
+# model = get_peft_model(model, lora_config)
+
+# model.to('cuda:0')
+print(model.device)
+
+def preprocess_function(example):
+    input = f"{example['question']} {example['process']}"
+    tokenized_inputs = tokenizer(
+        input, 
+        truncation=True, 
+        padding='max_length', 
+        # padding=True,
+        max_length=2048,
+    )
+    
+    def find_all_indices(lst, element):
+        return [i for i, x in enumerate(lst) if x == element]
+    
+    length = len(tokenized_inputs['input_ids'])
+    # print(length)
+    indices = find_all_indices(tokenized_inputs['input_ids'],step_tag_id)
+    
+    if len(indices) != len(example['label']):
+        # print(example)
+        example['label'] = example['label'][:len(indices)]
+    
+    assert len(indices) == len(example['label'])
+    
+    tokenized_inputs['labels'] = [-100] * length
+    # tokenized_inputs['attention_mask'] = [1] *length
+    # print(len(indices))
+    for i in range(len(indices)):
+        if example['label'][i] == '+' or example['label'][i] == 1:
+            tokenized_inputs['labels'][indices[i]] = candidate_tokens[0]
+        elif example['label'][i] == '-' or example['label'][i] == 0:
+            tokenized_inputs['labels'][indices[i]] = candidate_tokens[1]
+        else:
+            raise ValueError('label is wrong')
+        tokenized_inputs['attention_mask'][indices[i]] = 0
+    # tokenized_inputs['labels'] = [-100] *(length-1) + tokenized_inputs['input_ids'][length-1:]
+    
+    return tokenized_inputs
+
+DATA_PATH = {
+    "train": os.path.join(args.data_path, "graph_silo.json"),
+    
+}
+
+random.seed(42)
+
+dataset = load_dataset('json', data_files=DATA_PATH)
+# aps_length = len(dataset['train'])
+# dataset['train'] = dataset['train'].select(random.sample(range(aps_length),30000))
+
+# if args.datasets == 'both':
+#     dataset2 = load_dataset('json',data_files=os.path.join(args.data_path, "graph_silo_part_2.json"))
+
+
+print('start processing')
+tokenized_datasets = dataset.map(preprocess_function)
+tokenized_datasets['train'] = tokenized_datasets['train'].remove_columns(['question','process','label'])
+
+# tokenized_datasets['test'] = tokenized_datasets['test'].remove_columns(['question','process','label'])
+print(tokenized_datasets['train'])
+print('dataset processed')
+# print(tokenized_datasets['train']['input_ids'])
+# print(len(tokenized_datasets['train']['input_ids'][0]))
+
+# Data collator for padding inputs dynamically
+data_collator = DataCollatorWithPadding(tokenizer)
+
+BATCH_SIZE = args.total_batch_size
+GRADIENT_ACCUMULATION_STEPS = BATCH_SIZE // args.per_device_train_batch_size
+
+world_size = int(os.environ.get("WORLD_SIZE", 1))
+ddp = world_size != 1
+if ddp:
+    
+    GRADIENT_ACCUMULATION_STEPS = GRADIENT_ACCUMULATION_STEPS // world_size
+
+print(world_size)
+print(ddp)
+
+time = time.strftime("%Y-%m-%d-%H-%M", time.localtime())
+fp = f'bs_{args.total_batch_size}_lr_{args.learning_rate}_datasets_{args.datasets}'
+output_path = f'./prm_results_qwen_new.{args.server}.{time}/{fp}'
+
+
+# Training arguments
+training_args = TrainingArguments(
+    output_dir=output_path,
+    evaluation_strategy="no",  # Evaluate at the end of each epoch
+    learning_rate=args.learning_rate,
+    per_device_train_batch_size=args.per_device_train_batch_size,
+    per_device_eval_batch_size=args.per_device_eval_batch_size,
+    gradient_accumulation_steps=GRADIENT_ACCUMULATION_STEPS,
+    num_train_epochs=1,
+    weight_decay=0.01,
+    logging_dir="./logs",
+    logging_steps=10,  # 10
+    save_strategy="epoch",  # epoch
+    # save_steps=500,  
+    save_total_limit=1,  
+    # fp16=True,  # Enable mixed precision for better performance on supported hardware
+    bf16=True,
+    report_to="none",  # Set to "wandb" if you are using Weights and Biases for logging
+    dataloader_num_workers=4,
+    deepspeed="../config/deepspeed_config_stage3.json",  # None
+    ddp_find_unused_parameters=False,
+)
+
+# Define a custom metric function (e.g., accuracy for binary classification)
+def compute_metrics(eval_pred):
+    # pass
+    # print(eval_pred)
+    print('bb')
+    pre, labels = eval_pred
+    auc = roc_auc_score(pre[1], pre[0])
+    ll = log_loss(pre[1], pre[0])
+    acc = accuracy_score(pre[1], pre[0] > 0.5)
+    result ={
+        'auc': auc, 
+        'll': ll, 
+        'acc': acc, 
+    } 
+    print(result)
+    return result
+
+def preprocess_logits_for_metrics(logits,labels):
+    print('aa')
+    # return logits,labels
+    labels_index = torch.argwhere(torch.bitwise_or(labels == candidate_tokens[0], labels == candidate_tokens[1]))
+    gold = torch.where(labels[labels_index[:, 0], labels_index[:, 1]] == candidate_tokens[1], 0, 1)
+    # labels_index[: , 1] = labels_index[: , 1] - 1
+    logits = logits[labels_index[:, 0], labels_index[:, 1]][:, [candidate_tokens[1], candidate_tokens[0]]]
+    prob = torch.softmax(logits, dim=-1)
+    return prob[:, 1], gold
+    
+
+# Initialize the Trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_datasets['train'],
+    # eval_dataset=tokenized_datasets['test'],  # Replace with a validation set if available
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+    preprocess_logits_for_metrics=preprocess_logits_for_metrics,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+# trainer.evaluate()
+
+# Save the fine-tuned model and tokenizer
+model.save_pretrained('./fine_tuned_GraphPRM_sft')  # fine_tuned_math_shepherd_lora_8bit
+tokenizer.save_pretrained('./fine_tuned_GraphPRM_sft')
